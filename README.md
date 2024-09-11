24e94bfb4137ceecc15d28603e78885b5981b047
Miguel Fernández <miguel.fernandez008@gmail.com>
2024-09-11 23:16:02 +0200

    :construction: Help and fixes
    :books: Change log.
    :new: Gitignore bat and folder downloads.
    :new: Now videos download in a downloads folder.
    :new: Info prints.
    :new: Help command.

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # youtube_downloader
  A command line script to download youtube videos
+
+## List of available commands
+
+### -h
+Displays help
+```
+python main.py -h
+```
+### -u
+Download the youtube url passed as the next argument.
+```
+python main.py -u https://youtube.com/watch?v=12345
+```
+### -a
+Download the youtube url passed as the next argument. Audio only mode.
+```
+python main.py -a https://youtube.com/watch?v=12345
+```
+### -v.
+Download the youtube url passed as the next argument. Video only mode.
+```
+python main.py -v https://youtube.com/watch?v=12345
+```
+### -f 
+Download a list of urls written in a text file passed as the next argument. 
+```
+python main.py -f urls.txt
+```
+Every file we want to download from the text file must be in the following format: video_url,folder_name. 
+```
+https://youtube.com/watch?v=11111,FOLDER_NAME
+https://youtube.com/watch?v=22222,FOLDER_NAME
+```
--- main.py
+++ main.py
@@ -12,10 +12,25 @@ options = "hu:a:v:f:"
 # Long options
 long_options = []
 
+# Config
+parent_path = './downloads'
+file_extension = 'mp4'
+help_command_name = '-h'
+url_command_name = '-u'
+audio_command_name = '-a'
+video_command_name = '-v'
+file_command_name = '-f'
+
 def help_command():
-    print('help command')
+    print('{} = Displays help.'.format(help_command_name))
+    print('{} = Download the youtube url passed as the next argument.'.format(url_command_name))
+    print('{} = Download the youtube url passed as the next argument. Audio only mode.'.format(audio_command_name))
+    print('{} = Download the youtube url passed as the next argument. Video only mode.'.format(video_command_name))
+    print('{} = Download a list of urls written in a text file passed as the next argument. Every file we want to download from the text file must be in the following format: video_url,folder_name. Ex: https://youtube.com/watch?v=12345,FOLDER_NAME'.format(file_command_name))
+    print('For for more info go to see repository README file.')
 
 def file_command(file):
+    print('Starting file download...')
     try:
         file = open(file=file, mode='rt')
         for index, line in enumerate(file):
@@ -23,8 +38,7 @@ def file_command(file):
             if len(values) < 2:
                 print('Line {} has not correct format'.format(index + 1))
                 continue
-            url = values[0] or None
-            parent_path = './'
+            url = values[0] or None            
             path = parent_path + values[1] or parent_path
             path_stripped = path.strip()
             if path is not None and not os.path.exists(path_stripped):
@@ -36,7 +50,6 @@ def file_command(file):
         print(str(err))
 
 def url_command(url, output_path, only_audio = False, only_video=False):
-    file_extension = 'mp4'
     try: 
         YouTube(url, on_progress_callback=on_progress, on_complete_callback=on_complete_callback) \
             .streams \
@@ -48,9 +61,11 @@ def url_command(url, output_path, only_audio = False, only_video=False):
         print(error)
 
 def audio_only_command(url):
+    print('Starting audio only download...')
     url_command(url=url, only_audio=True)
 
 def video_only_command(url):
+    print('Starting video only download...')
     url_command(url=url, only_video=True)
 
 def on_complete_callback(stream, file_path):
@@ -59,15 +74,16 @@ def on_complete_callback(stream, file_path):
 try:
     args, values = getopt.getopt(new_arguments, options, long_options)
     for current, value in args:
-        if(current in ('-h')):
+        if(current in (help_command_name)):
             help_command()
-        elif(current in('-u')):
+        elif(current in(url_command_name)):
+            print('Starting url download...')
             url_command(url=value)
-        elif(current in('-a')):
+        elif(current in(audio_command_name)):
             audio_only_command(url=value)
-        elif(current in('-v')):
+        elif(current in(video_command_name)):
             video_only_command()
-        elif(current in ('-f')):
+        elif(current in (file_command_name)):
             file_command(file=value)
 except getopt.error as err:
     print(str(err))
